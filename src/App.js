709c9da0fcba79a0c71f0d40650009815f9e0ede
saikumar2912 <ksai4666@gmail.com>
2021-07-20 13:06:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
-import Pagination from './Components/Pagination';
+import Paginate from './Components/Pagination';
 import {useSelector,useDispatch} from 'react-redux'
 import {Page} from './Redux/Actions'
 
 function App() {
   const state = useSelector(state => state.data)
+  console.log(state)
 const dispatch=useDispatch()
-  const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(5);
+  const [DataPerPage] = useState(5);
   useEffect(() => {
-
     dispatch(Page())
-}, [setPosts])
+
+}, [])
 
 
-console.log(posts)
-const indexOfLastPost = currentPage * postsPerPage;
-const indexOfFirstPost = indexOfLastPost - postsPerPage;
-const currentPosts = state.slice(indexOfFirstPost, indexOfLastPost);
+const indexOfLastData = currentPage * DataPerPage;
+const indexOfFirstData = indexOfLastData - DataPerPage;
+const currentData = state.slice(indexOfFirstData, indexOfLastData);
 const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <div className="App">
-    <Pagination posts={currentPosts} 
-     postsPerPage={postsPerPage}
-        totalPosts={posts.length}
+    <Paginate Data={currentData} 
+     DataPerPage={DataPerPage}
+        totalPosts={state.length}
         paginate={paginate}/>
           </div>
   );
--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,29 +1,70 @@
-import React,{useEffect} from 'react'
-const Pagination = ({ postsPerPage, totalPosts, paginate,posts }) => {
+
+import React from 'react'
+
+const Paginate = ({ DataPerPage, totalPosts, paginate,Data }) => {
   
 
     const pageNumbers = [];
-console.log(posts)
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+console.log(Data)
+  for (let i = 1; i <= Math.ceil(totalPosts / DataPerPage); i++) {
     pageNumbers.push(i);
   }
 
    
     return (
         <div>
+            <tbody>
+            <th> name</th>
+                <th> abv</th>
+                <th> attenuation</th>
+                <th>brewers_tips</th>
+                <th>contributed_by</th>
+                <th> boil_volume</th>
+                <th>description </th>
+                <th>food_pairing </th>
+                <th>tagline</th>
+
+
+      {Data.map((item, i) => {
+        return (
+          <>
+            <tr key={i} >
+              
+              <td>{item.name}</td>
+              <td>{item.abv}</td>
+              <td>{item.attenuation_level}</td>
+              <td>{item.brewers_tips}</td>
+              <td> {item.contributed_by}</td>
+              <td> {item.boil_volume.value},{item.boil_volume.unit}</td>
+              <td>{item.description}</td>
+              <td>{item.food_pairing.map(e=>e)} </td>
+              <td>{item.tagline}</td>
+
+            </tr>
+           
+          </>
+        );
+      })}
+    </tbody>
          <nav>
-      <ul className='pagination'>
-        {pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link'>
-              {number}
+         
+      <ul className=''>
+
+        {pageNumbers.map(number => (<>
+            
+            <a key={number} onClick={() => paginate(number)} href='!#' className='page-link'>
+                {number}
+
             </a>
-          </li>
+
+            </>
         ))}
+
       </ul>
+      
     </nav>
         </div>
     )
 }
 
-export default Pagination
+export default Paginate
